394c15d6fc998da3ad02061ac34dc9a574c8edce
eusebiumustea <musteaeusebiu14@gmail.com>
2024-04-01 01:48:39 +0300

    added web, mobile, api

---
+++ api/src/main/java/com/realtyranger/api/RealtyRangerApplication.java
@@ -0,0 +1,13 @@
+package com.realtyranger.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealtyRangerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealtyRangerApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/com/realtyranger/api/RealtyRangerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.realtyranger.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealtyRangerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
